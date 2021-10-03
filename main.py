13fad2665f53160c31f3171552bdb28dd036cfb3
Manoel <mfmfel@gmail.com>
2021-10-02 23:36:16 -0300

    Aula 29/09 0.0.3

--- main.py
+++ main.py
@@ -1,114 +1,130 @@
-# imports - bibliotecas
+# 1 - imports - bibliotecas
 import pytest
-# 2 - class - classes
+# 2 - class - classe
 
 # 3 - definitions - definições = métodos e funções
-
-# Cada "def" é um metódo
 def print_hi(name):
     print(f'Oi, {name}')
 
-def somar (num1, num2):
-    return num1 + num2
 
-def subtrair  (num1, num2):
-    return num1 - num2
+def somar(numero1, numero2):
+    return numero1 + numero2
+
+def subtrair(numero1, numero2):
+    return numero1 / numero2 # bug!!
+
+def multiplicar(numero1, numero2):
+    return numero1 * numero2
 
 # esse é um exemplo de demonstração
-'''
-def dividir(num1, num2):
-    if num1 != 0:
-        return num1 / num2
+def dividir(numero1, numero2):
+    if numero2 != 0:
+        return numero1 / numero2
     else:
-        return 'Não dividirás por 0'
+        return 'Não dividirás por zero'
 
-
-def dividir_zero(num1, num2):
-    return num1 / num2
-'''
-
-def dividir_try_except(num1, num2):
+def dividir_try_except(numero1, numero2):
     try:
-        return num1 / num2
-    except:
-        return 'Não dividirás por 0'
+        return numero1 / numero2
+    except TypeError:
+        #return 'Não dividirás por zero'
+        if TypeError == ZeroDivisionError:
+            return 'Não dividirás por zero'
+        elif TypeError == ArithmeticError:
+            return 'Erro no cálculo'
+        elif TypeError == ValueError:
+            return 'Erro no valor'
+        else:
+            return 'Erro desconhecido'
+        pass
 
-def multiplicar  (num1, num2):
-    return num1 * num2
+# Testes Unitarios / Teste de Unidades
 
+ # teste da função de somar
+def test_somar_didatico():
+    # 1 - Configura / Prepara
+    numero1 = 8 # input / entrada
+    numero2 = 5 # input / entrada
+    resultado_esperado = 13 # output / saida
+    # 2 - Executa
+    resultado_atual = somar(numero1,numero2)
+    # 3 - Check / Valida
+    assert resultado_atual == resultado_esperado
 
-# Testes unitários / Testes de Unidades
-
-# Testes da função somar
-
-@pytest.mark.parametrize('num1, num2, resultado',[
+@pytest.mark.parametrize('numero1,numero2,resultado',[
     #valores
-    (5, 4, 9),# testes 1
-    (3, 3, 6),# testes 2
-    (10, 6, 16),# testes 3
+    (5, 4, 9), # teste 1
+    (3, 2, 5), # teste 2
+    (10,6, 16), # teste 3
 ])
-def test_somar(num1, num2, resultado):
+def test_somar(numero1, numero2, resultado):
     try:
-        assert somar(num1, num2) == resultado
+        assert somar(numero1,numero2) == resultado
     except AssertionError:
         print(f'Entrou no Except: {AssertionError}')
-
-def test_somar():
-    # 1 - Configura / Prepara
-    num1 = 8 # input / entrada
-    num2 = 5 # input / entrada
-    resultado_esrepado = 13 # output / saida
-
-    # 2 - Executa
-    resultado_atual = somar(num1,num2)
-
-    # 3 - Check / Valida
-    assert resultado_atual == resultado_esrepado
-
-#o mesmo teste só que resumido
-def test_somar_compacto():
-    assert somar(8,5) == 13
+        pass
 
 def test_somar_resultado_negativo():
     assert somar(-1000,-2000) == -3000
 
-def teste_subtrair():
+def test_subtrair():
     assert subtrair(4,5) == -1
 
-def teste_multiplicar():
+def test_multiplicar():
     assert multiplicar(3,7) == 21
-'''
-def testes_dividir():
+
+def test_dividir():
     assert dividir(8,4) == 2
 
-def testes_dividir_por_zero():
-    assert dividir_zero(8,0) == 'Não dividirás por 0'
-'''
-@pytest.mark.parametrize('num1, num2, resultado',[
-    (8, 2, 4),
-    (20, 4, 5),
-    (10, 0, 'Não dividirás por 0'),
+def test_dividir_por_zero():
+    assert dividir(8,0) == 'Não dividirás por zero'
+
+
+
+@pytest.mark.parametrize('numero1, numero2, resultado',[
+    (8,2,4),
+    (20,4,5),
+    (10,0,'Não dividirás por zero')
 ])
-def testes_dividir_try_except(num1, num2, resultado):
-    assert dividir_try_except(num1, num2, resultado)) == 'Não dividirás por 0'
+def test_dividir_try_except(numero1,numero2,resultado):
+    assert dividir_try_except(numero1,numero2) == resultado
+
+
+
+
+
+
+    # teste positivo --> mostrar o resultado correto
+    #                --> avançar para a próxima etapa
+
+    # teste negativo --> mostrar a mensagem de erro
+
+# Dia 1 : 100 testes : 0 passaram
+# Dia 2 : 100 testes : 5 passaram
+# Dia 3 : 100 testes : 15 passaram
+# Dia 4 : 100 testes : 30 passaram
+
+#TDD : Desenvolvimento Direcionado pelo Testes
+# - Criar o esqueleto de classes, funções e métodos logo no início da Sprint
+# - Criar pelo 1 teste (feliz) para todas as funções e métodos
+# - Executar todos os testes unitários diariamente para medir o progresso
+
 
 if __name__ == '__main__':
-    print_hi('Manoel')
+    print_hi('Jose')
 
-    resultado = somar(4, 2)
-    print(f'O resultado de somar é: {resultado}')
+    # soma de 2 números
+    resultado = somar(4,2)
+    print(f'O resultado da soma: {resultado}')
 
-    resultado = subtrair(14, 23)
-    print(f'O resultado de subtrair é: {resultado}')
+    # subtração de 2 números
+    resultado = subtrair(5,3)
+    print(f'O resultado da subtração: {resultado}')
 
-    resultado = dividir(4, 782)
-    print(f'O resultado de dividir é: {resultado}')
+    # multiplicação
+    resultado = multiplicar(2,4)
+    print(f'O resultado da multiplicação: {resultado}')
 
-    resultado = dividir_zero(0, 782)
-    print(f'O resultado de dividir é: {resultado}')
-
-    resultado = multiplicar(14, 211)
-    print(f'O resultado de multiplicar é: {resultado}')
-
-
-
+    # divisão
+    resultado = dividir(9,8)
+    print(f'O resultado da divisão: {resultado}')
--- usuario.py
+++ usuario.py
@@ -1,4 +1,6 @@
 # 1 - imports
+import json
+
 import pytest
 import csv
 
@@ -24,30 +26,71 @@ teste_dados_usuarios_atuais = [
 
 @pytest.mark.parametrize('id,nome,sobrenome,email', teste_dados_usuarios_atuais)
 def testar_dados_usuarios(id,nome,sobrenome,email): # função que testa o algo
-   try:
-       response = requests.get(f'https://reqres.in/api/users/{id}')
-       jsonResponse = response.json()
-       id_obtido = jsonResponse['data']['id']
-       nome_obtido = jsonResponse['data']['first_name']
-       sobrenome_obtido = jsonResponse['data']['last_name']
-       email_obtido = jsonResponse['data']['email']
+    try:
+        response = requests.get(f'https://reqres.in/api/users/{id}')
+        jsonResponse = response.json()
+        id_obtido = jsonResponse['data']['id']
+        nome_obtido = jsonResponse['data']['first_name']
+        sobrenome_obtido = jsonResponse['data']['last_name']
+        email_obtido = jsonResponse['data']['email']
 
-       print(f'id: {id_obtido} \n nome: {nome_obtido} \n sobrenome: {sobrenome_obtido} \n email: {email_obtido}')
-       print(f'id: {id_obtido} - nome: {nome_obtido} - sobrenome: {sobrenome_obtido} - email: {email_obtido}')
-       print('id:{} \n nome:{} \n sobrenome:{} \n email:{}'.format(id_obtido, nome_obtido, sobrenome_obtido, email_obtido))
+        print(f'id: {id_obtido} \n nome: {nome_obtido} \n sobrenome: {sobrenome_obtido} \n email: {email_obtido}')
+        print(f'id: {id_obtido} - nome: {nome_obtido} - sobrenome: {sobrenome_obtido} - email: {email_obtido}')
+        print('id:{} \n nome:{} \n sobrenome:{} \n email:{}'.format(id_obtido, nome_obtido, sobrenome_obtido, email_obtido))
+        print(json.dumps(jsonResponse, indent=2, sort_keys=True))
 
-       assert id_obtido == id
-       assert nome_obtido == nome
-       assert sobrenome_obtido == sobrenome
-       assert email_obtido == email
+        assert id_obtido == id
+        assert nome_obtido == nome
+        assert sobrenome_obtido == sobrenome
+        assert email_obtido == email
 
-   except HTTPError as http_fail : # Para o ISTQB, descobriu rodando é falha
-       print(f'Um erro de HTTP aconteceu: {http_fail}')
-   except Exception as fail:        # Qualquer exceção será tratada a seguir
-       print(f'Falha inesperada: {fail}')
+    except HTTPError as http_fail : # Para o ISTQB, descobriu rodando é falha
+        print(f'Um erro de HTTP aconteceu: {http_fail}')
+    except Exception as fail:        # Qualquer exceção será tratada a seguir
+        print(f'Falha inesperada: {fail}')
 
 
 #função que faz algo --> Fora do meu computador
 # API que vamos usar para fazer o teste:
 # https://reqres.in/api/users/{id}
 
+# Leitor do Arquivo CSV
+def ler_dados_do_csv():
+    teste_dados_csv = []
+    nome_arquivo = 'usuarios.csv'
+    try:
+        with open(nome_arquivo,newline='') as csvfile:
+            dados = csv.reader(csvfile,delimiter=',')
+            next(dados)
+            for linha in dados:
+                teste_dados_csv.append(linha)
+        return teste_dados_csv
+    except FileNotFoundError:
+        print(f'Arquivo não encontrado: {nome_arquivo}')
+    except Exception as fail:
+        print(f'Falha imprevista: {fail}')
+
+@pytest.mark.parametrize('id,nome,sobrenome,email', ler_dados_do_csv() )
+def testar_dados_usuarios_csv(id,nome,sobrenome,email): # função que testa o algo
+    try:
+        response = requests.get(f'https://reqres.in/api/users/{id}')
+        jsonResponse = response.json()
+        id_obtido = jsonResponse['data']['id']
+        nome_obtido = jsonResponse['data']['first_name']
+        sobrenome_obtido = jsonResponse['data']['last_name']
+        email_obtido = jsonResponse['data']['email']
+
+        print(f'id: {id_obtido} \n nome: {nome_obtido} \n sobrenome: {sobrenome_obtido} \n email: {email_obtido}')
+        print(f'id: {id_obtido} - nome: {nome_obtido} - sobrenome: {sobrenome_obtido} - email: {email_obtido}')
+        print('id:{} \n nome:{} \n sobrenome:{} \n email:{}'.format(id_obtido, nome_obtido, sobrenome_obtido, email_obtido))
+        print(json.dumps(jsonResponse, indent=2, sort_keys=True))
+
+        assert id_obtido == int(id)
+        assert nome_obtido == nome
+        assert sobrenome_obtido == sobrenome
+        assert email_obtido == email
+
+    except HTTPError as http_fail : # Para o ISTQB, descobriu rodando é falha
+        print(f'Um erro de HTTP aconteceu: {http_fail}')
+    except Exception as fail:        # Qualquer exceção será tratada a seguir
+        print(f'Falha inesperada: {fail}')
